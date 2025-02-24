87d50e487a839a3c11b25a9684c63bae132c3504
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-02-24 21:30:38 +0100

    update

--- controllers.php
+++ controllers.php
@@ -7,32 +7,12 @@ function list_action()
     $posts = get_all_posts();
 
     require __DIR__ . '/templates/list.html.php';
-
-    $html = render_template('templates/list.html.php', [
-        'posts' => $posts,
-    ]);
-
-    return new Response($html);
 }
 
 function show_action($id)
 {
     $post = get_post_by_id($_REQUEST['id']);
-    $html = render_template('templates/show.html.php', [
-        'post' => $post,
-    ]);
-
-    return new Response($html);
+    require __DIR__ . '/templates/show.html.php';
 }
 
-function render_template($path, array $args)
-{
-    extract($args);
-    ob_start();
-    require __DIR__ . '/' . $path;
-    $html = ob_get_clean();
 
-    return $html;
-}
-
-// based on https://symfony.com/doc/current/introduction/from_flat_php_to_symfony.html
